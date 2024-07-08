f36da9d91e5ade45eacf482b808cb80837c89f66
rb9507 <rajbhutekop555@gmail.com>
2024-07-08 22:07:28 +0530

    log change in db.on

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const Ebikes = path.join(__dirname, '/temp/Ebikes.html');
 
 mongoose.connect(process.env.DB_CONN_STRING);
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', console.error.bind(console, 'found connection error:'));
 db.once('open', function () {
     console.log('We are connected!!')
 });
